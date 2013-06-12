1464fda3e1f1c32e02a581e38e5db73fdfa449b1
CoombeT <tim.coombe@bskyb.com>
2013-06-12 13:57:04 +0100

    Added tests for service

--- test/unit/com/dycon/DynamicContentServiceTests.groovy
+++ test/unit/com/dycon/DynamicContentServiceTests.groovy
@@ -1,17 +1,44 @@
 package com.dycon
 
+import grails.test.mixin.Mock
+import grails.test.mixin.TestFor
 
 
-import grails.test.mixin.*
-import org.junit.*
-
-/**
- * See the API for {@link grails.test.mixin.services.ServiceUnitTestMixin} for usage instructions
- */
 @TestFor(DynamicContentService)
+@Mock([DynamicContent, DynamicContentPage])
 class DynamicContentServiceTests {
 
-    void testSomething() {
-        //
+    void setUp(){
+        def home = new DynamicContentPage(name:"home",path: "").save()
+        new DynamicContent(name:"content-1",value:"content-1-non-live-value",live:false,page: home).save()
+        new DynamicContent(name:"content-2",value:"content-2-non-live-value",live:false,page: home).save()
+        new DynamicContent(name:"content-1",value:"content-1-live-value",live:true,page: home).save()
+        new DynamicContent(name:"content-2",value:"content-2-live-value",live:true,page: home).save()
     }
+
+   void testGetContentForNonExistentPageReturnsEmptyMap(){
+
+
+      def content = service.getPageContent("nopage",false)
+      assertEquals(0, content.size())
+
+   }
+
+   void testGetNonLiveContentGetsCorrectResults(){
+
+       def content = service.getPageContent("home",false)
+       assertEquals("content-1-non-live-value", content['content-1'])
+       assertEquals("content-2-non-live-value", content['content-2'])
+
+   }
+
+    void testGetLiveContentGetsCorrectResults(){
+
+        def content = service.getPageContent("home",true)
+        assertEquals("content-1-live-value", content['content-1'])
+        assertEquals("content-2-live-value", content['content-2'])
+
+    }
+
+
 }
